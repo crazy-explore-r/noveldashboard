ad27412157da5e172026b583703ddb94b0634572
crazy-explore-r <developer.ragul@gmail.com>
2024-04-25 13:29:46 +0530

    feat: Initialize App

---
+++ frontend/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import frappeui from 'frappe-ui/vite'
+import proxyOptions from './proxyOptions';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 8080,
+    proxy: proxyOptions
+  },
+  plugins: [frappeui(), vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: `../${path.basename(path.resolve('..'))}/public/frontend`,
+    emptyOutDir: true,
+    target: 'es2015',
+  },
+  optimizeDeps: {
+    include: ['frappe-ui > feather-icons', 'showdown', 'engine.io-client'],
+  },
+})
